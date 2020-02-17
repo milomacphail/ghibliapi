5bd9b6fbef15dfb7d91856b93c69b59db5ad2779
milomacphail <milomacphail@gmail.com>
2020-02-16 19:38:00 -0500

    Displayed to DOM; unstyled; XMLHTTP

--- script.js
+++ script.js
@@ -1,14 +1,37 @@
-var api = "https://ghibliapi.herokuapp.com/films"
+var request = new XMLHttpRequest()
 
-$(document).ready(function(){
-    $.getJSON(api, function(data){
-        console.log(data[0]);
-    });
-});
+request.open('GET', 'https://ghibliapi.herokuapp.com/films', true)
+request.onload = function() {
+
+  var data = JSON.parse(this.response)
+
+  if (request.status >= 200 && request.status < 400) {
+    data.forEach(movie => {
+        const card = document.createElement('div')
+        card.setAttribute('class', 'card')
+
+        const h1 = document.createElement('h1')
+        h1.textContent = movie.title
+
+        const p = document.createElement('p')
+        movie.description = movie.description.substring(0, 300)
+        p.textContent = `${movie.description}...`
+
+        container.appendChild(card)
+
+        card.appendChild(h1)
+        card.appendChild(p)
+    })
+  } else {
+    console.log('error')
+  }
+}
+
+request.send()
 
 const app = document.getElementById('main');
 
-const logo = document.createElement('logo');
+const logo = document.createElement('img');
 
 logo.src = 'logo.png';
 
